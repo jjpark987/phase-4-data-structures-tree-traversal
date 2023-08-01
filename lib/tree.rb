80142de674a1064f4476ae957fbeef50244fcf9b
Jason Park <jjpark987@gmail.com>
2023-08-01 12:35:07 -0700

    commit tree 0341f37af2ff063f148b2f7f7322428c41e39328

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,5 +1,46 @@
+require 'pry'
+
 class Tree
   def initialize(root = nil)
     @root = root
+  end
+
+  # depth-first search
+  def get_element_by_id string
+    # depth_first_traversal(@root).each do |node|
+    #   return node if node[:id] == string
+    # end
+    breadth_first_traversal(@root).each do |node|
+      return node if node[:id] == string
+    end
+    nil
+  end
+
+  def depth_first_traversal node
+    result = []
+    nodes_to_visit = [node]
+
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      result.push(node)
+      # add child nodes to the beginning of nodes_to_visit
+      nodes_to_visit = node[:children] + nodes_to_visit
+    end
+
+    result
+  end
+
+  def breadth_first_traversal node
+    result = []
+    nodes_to_visit = [node]
+  
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      result.push(node) 
+      # add child nodes to the end of nodes_to_visit
+      nodes_to_visit = nodes_to_visit + node[:children]
+    end
+  
+    result
   end
 end
